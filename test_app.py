db460adb717404308a9249c91fae4a674b18fb24
Joaoc <joaocarralcontato@gmail.com>
2026-06-26 19:43:34 -0300

    Script de teste pronto

--- test_app.py
+++ test_app.py
@@ -1,35 +1,52 @@
-from app import app
+from main import app
 
+client = app.test_client()
+
+# Teste para consultar todos os livros
 def test_obter_livros():
-    response = app.test_client().get('/livros')
+    response = client.get('/livros')
+
     assert response.status_code == 200
     assert len(response.get_json()) == 3
 
+
+# Teste para consultar um livro pelo ID
 def test_obter_livro_por_id():
-    response = app.test_client().get('/livros/1')
+    response = client.get('/livros/1')
+
     assert response.status_code == 200
     assert response.get_json()['titulo'] == 'O Senhor dos Anéis'
 
+
+# Teste para criar um livro
 def test_criar_livro():
-    cliente = app.test_client()
     novo_livro = {
         'id': 4,
         'titulo': 'Novo Livro',
         'autor': 'Autor',
     }
-    response = cliente.post('/livros', json=novo_livro)
+
+    response = client.post('/livros', json=novo_livro)
+
     assert response.status_code == 201
-    assert response.get_json() == novo_livro
 
+    data = response.get_json()
+
+    assert data['titulo'] == 'Novo Livro'
+    assert data['autor'] == 'Autor'
+
+
+# Teste para editar um livro pelo ID
 def test_editar_livro():
-    client = app.test_client()
-
     dados = {
         'titulo': 'Livro Editado',
         'autor': 'Autor Editado'
     }
-    response = client.put('/livros/1', json=dados)
-    assert response.status_code == 200
-    livro = response.get_json()
-    assert livro['titulo'] == 'Livro Editado'
 
+    response = client.put('/livros/1', json=dados)
+
+    assert response.status_code == 200
+
+    livro = response.get_json()
+
+    assert livro['titulo'] == 'Livro Editado'
