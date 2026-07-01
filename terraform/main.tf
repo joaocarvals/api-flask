862b822c01082b326933f0af9b2983ec228ef565
Joaoc <joaocarralcontato@gmail.com>
2026-07-01 18:24:35 -0300

    estrutura main ajustada

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,22 +9,14 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "flask_api" {
-  name = "flask-api:latest"
-
-  build {
-    context = ".."
-    dockerfile = "./Dockerfile"
-  }
-}
-
 resource "docker_container" "flask_api_container" {
   name  = "flask-api-container"
-
-  image = docker_image.flask_api.image_id
+  image = "flask-api:latest"
 
   ports {
     internal = 5000
     external = 5000
   }
+
+  restart = "unless-stopped"
 }
