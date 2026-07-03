abf630aee87fb3824f2786fddd902dc78594d7f4
Joaoc <joaocarralcontato@gmail.com>
2026-07-03 10:52:08 -0300

    coments in main tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
+# Define as configurações necessárias para o Terraform
 terraform {
+
+  # Especifica os providers utilizados no projeto
   required_providers {
+
+    # Provider responsável por gerenciar recursos do Docker
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 3.0"
@@ -7,16 +12,30 @@ terraform {
   }
 }
 
+# Configura a conexão com o Docker local
 provider "docker" {}
 
+# Cria um container Docker para executar a aplicação Flask
 resource "docker_container" "flask_api_container" {
-  name  = "flask-api-container"
+
+  # Nome que será atribuído ao container
+  name = "flask-api-container"
+
+  # Imagem Docker que será utilizada para criar o container
   image = "flask-api:latest"
 
+  # Mapeamento de portas entre o host e o container
   ports {
+
+    # Porta utilizada pela aplicação dentro do container
     internal = 5000
+
+    # Porta exposta na máquina host
     external = 5000
   }
 
+  # Define a política de reinicialização do container
+  # O container será reiniciado automaticamente,
+  # exceto quando for parado manualmente.
   restart = "unless-stopped"
 }
