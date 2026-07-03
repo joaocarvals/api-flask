21325f7c6cbbf479065cee9a4b31253894b8e3af
Joaoc <joaocarralcontato@gmail.com>
2026-07-03 10:19:12 -0300

    ajustes main #

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ def excluir_livro(id):
             del livros[indice]
             return jsonify(livros)
 
+#Para rodar a aplicação
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
